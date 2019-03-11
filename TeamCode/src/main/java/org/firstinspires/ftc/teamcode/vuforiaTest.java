a7e5fc64367eea2d6b1cb94dc54252ae014db25c
hk21702 <hk21702@gmail.com>
2019-03-11 11:17:47 -0400

    Vuforia Test WIP

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vuforiaTest.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.BACK;
+import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.FRONT;
+
+@Autonomous(name = "VuForia Test")
+
+public class VuforiaTest extends LinearOpMode {
+    // Vuforia license key
+    private static final String VUFORIA_Key = ""; 
+
+    // Field dimensions
+    private static final float mmPerInch = 25.4f;
+    private static final float mmFieldWidth = (12 * 6) * mmPerInch;
+    private static final float mmTargetHeight = (6) * mmPerInch;
+
+    //Camera being used on phone
+    private static final VuforiaLocalizer.CameraDirection CAMERA_CHOICE = Back;
+
+    private OpenGLMatrix lastLocation = null;
+    private boolean targetVisible = false;
+
+    //Localization engine variable
+    VuforiaLocalizer vuforia;
+
+    @Override public void runOpMode(){
+        //Camera Preview Paramater object creation
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id",
+                hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        parameters.vuforiaLicenseKey = VUFORIA_Key;
+        parameters.CameraDirection = CAMERA_CHOICE;
+
+        //Instance Vuforia engine
+        vuforia = ClassFactory.getinstance().createVuforia(parameters);
+
+        
+    }
+}
